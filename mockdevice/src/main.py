29dca193c87e11fc4c90dc80655aa84f13ab51b3
Etienne Rebetez <etienne.rebetez@gmail.com>
2020-03-14 19:51:19 +0100

    Fix issues with python mock client

--- mockdevice/src/main.py
+++ mockdevice/src/main.py
@@ -20,8 +20,6 @@ DEVICE = {
     }
 }
 
-light_state = 0
-
 
 def on_open(ws):
     print("Connected to " + HOST + ":" + PORT)
@@ -51,20 +49,29 @@ def on_close(ws):
     # thread.start_new_thread(init, ())
 
 
-def on_message(ws, message):
-    print(message)
-    data = json.loads(message)
+def messageHandler():
+    light_state = 0
 
-    if data["key"] == "light_bottom":
-        oldValue = light_state
-        light_state = data["value"]
-        if oldValue != light_state:
-            ws.send(
-                json.dumps({
-                    "key": "light_bottom",
-                    "transaction_id": data["transaction_id"],
-                    "value": light_state
-                }))
+    def on_message(ws, message):
+        nonlocal light_state
+
+        print(message)
+        data = json.loads(message)
+
+        if data["key"] == "light_bottom":
+            oldValue = None
+            if data["value"] is not None:
+                oldValue = light_state
+                light_state = data["value"]
+
+            if oldValue != light_state:
+                ws.send(
+                    json.dumps({
+                        "key": "light_bottom",
+                        "transaction_id": data.get("transaction_id"),
+                        "value": light_state
+                    }))
+    return on_message
 
 
 def saw_down():
@@ -84,14 +91,21 @@ def saw_down():
     return new_val
 
 
-t = threading.Timer(6, saw_down())
-t.start()
+def set_interval(func, sec):
+    def func_wrapper():
+        set_interval(func, sec)
+        func()
+    t = threading.Timer(sec, func_wrapper)
+    t.start()
+    return t
+
 
 if __name__ == "__main__":
     websocket.enableTrace(False)
     ws = websocket.WebSocketApp("ws://" + HOST + ":" + PORT,
-                                on_message=on_message,
+                                on_message=messageHandler(),
                                 on_error=on_error,
                                 on_close=on_close)
     ws.on_open = on_open
+    set_interval(saw_down(), 6)
     ws.run_forever()
