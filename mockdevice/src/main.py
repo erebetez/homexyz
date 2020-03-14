203c057374d997f55a282544ffb2fe7dc6b0cf73
Etienne Rebetez <etienne.rebetez@gmail.com>
2020-03-14 18:52:53 +0100

    port mock device to python

--- mockdevice/src/main.py
+++ mockdevice/src/main.py
@@ -4,32 +4,94 @@ try:
 except ImportError:
     import _thread as thread
 import time
+import os
+import json
+import threading
 
-def on_message(ws, message):
-    print(message)
+HOST = os.environ.get('host') or "localhost"
+PORT = os.environ.get('port') or "3667"
+
+DEVICE = {
+    "id": "m2",
+    "name": "mock device in python",
+    "states": {
+        "saw_down_mock": {"location": "livingroom", "unit": "rpm"},
+        "light_bottom": {"location": "livingroom", "type": "switch"}
+    }
+}
+
+light_state = 0
+
+
+def on_open(ws):
+    print("Connected to " + HOST + ":" + PORT)
+
+    ws.send(
+        json.dumps({
+            "key": "device",
+            "value": DEVICE
+        }))
+
+    ws.send(
+        json.dumps({
+            "key": "register",
+            "value": {
+                "id": DEVICE["id"],
+                "key": "light_bottom"
+            }
+        }))
+
 
 def on_error(ws, error):
     print(error)
 
+
 def on_close(ws):
     print("### closed ###")
+    # thread.start_new_thread(init, ())
 
-def on_open(ws):
-    def run(*args):
-        for i in range(3):
-            time.sleep(1)
-            ws.send("Hello %d" % i)
-        time.sleep(1)
-        ws.close()
-        print("thread terminating...")
-    thread.start_new_thread(run, ())
 
+def on_message(ws, message):
+    print(message)
+    data = json.loads(message)
+
+    if data["key"] == "light_bottom":
+        oldValue = light_state
+        light_state = data["value"]
+        if oldValue != light_state:
+            ws.send(
+                json.dumps({
+                    "key": "light_bottom",
+                    "transaction_id": data["transaction_id"],
+                    "value": light_state
+                }))
+
+
+def saw_down():
+    saw = 100
+
+    def new_val():
+        nonlocal saw
+        saw = saw - 1
+        if saw < 0:
+            saw = 100
+
+        ws.send(
+            json.dumps({
+                "key": "saw_down_mock",
+                "value": saw
+            }))
+    return new_val
+
+
+t = threading.Timer(6, saw_down())
+t.start()
 
 if __name__ == "__main__":
-    websocket.enableTrace(True)
-    ws = websocket.WebSocketApp("ws://echo.websocket.org/",
-                              on_message = on_message,
-                              on_error = on_error,
-                              on_close = on_close)
+    websocket.enableTrace(False)
+    ws = websocket.WebSocketApp("ws://" + HOST + ":" + PORT,
+                                on_message=on_message,
+                                on_error=on_error,
+                                on_close=on_close)
     ws.on_open = on_open
-    ws.run_forever() 
+    ws.run_forever()
