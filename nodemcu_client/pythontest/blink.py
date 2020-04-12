5787078b201aae18f74ca7023c2ffe895c6585e2
Etienne Rebetez <etienne.rebetez@gmail.com>
2020-04-12 14:28:15 +0200

    add test with micropython

---
+++ nodemcu_client/pythontest/blink.py
@@ -0,0 +1,26 @@
+from machine import Pin
+from machine import Timer
+
+
+def blinker():
+    print('setup blinker')
+    p0 = Pin(2, Pin.OUT)
+    blink_state = 0
+
+    def cb(args):
+        nonlocal blink_state
+        print('update blink')
+        print(blink_state)
+        if blink_state == 0:
+            p0.off()
+            blink_state = 1
+        else:
+            p0.on()
+            blink_state = 0
+
+    return cb
+
+
+blink_timer = Timer(-1)
+
+blink_timer.init(period=2000, mode=Timer.PERIODIC, callback=blinker())
